197cff0ab04d635c41b0254b77cf3a4cb9c161e4
Lloyd Montgomery <lloydrmontgomery@gmail.com>
2014-09-22 21:05:27 -0700

    Deleted misplaced files;  Finally on a working branch

--- basicopenglview.cpp
+++ basicopenglview.cpp
@@ -12,7 +12,6 @@ BasicOpenGLView::BasicOpenGLView(QWidget *parent)
     //polygons = QVector< QVector<QVector3D> >();
     srand (time(NULL));
     newPoly();
-    qDebug() << "NEW HERE";
 }
 
 void BasicOpenGLView::initializeGL()
