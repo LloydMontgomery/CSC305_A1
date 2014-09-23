9712c8504ad05dd301ce9c5bd8d8915221863d3b
Lloyd Montgomery <lloydrmontgomery@gmail.com>
2014-09-22 21:10:39 -0700

    no message

--- basicopenglview.cpp
+++ basicopenglview.cpp
@@ -12,7 +12,6 @@ BasicOpenGLView::BasicOpenGLView(QWidget *parent)
     //polygons = QVector< QVector<QVector3D> >();
     srand (time(NULL));
     newPoly();
-
 }
 
 void BasicOpenGLView::initializeGL()
